e027972a310f26a3e583f91f01b11afb17fe9f27
ianlau <ianlauyin@gmail.com>
2024-10-08 15:35:38 +0800

    Use Res instead of Component for health

--- src/game/stats/health.rs
+++ src/game/stats/health.rs
@@ -3,39 +3,34 @@ use bevy::prelude::*;
 
 use crate::states::{AppState, GameState};
 
-const INITIAL_HEALTH: u8 = 5;
+const INITIAL_HEALTH: u8 = 3;
 
 pub struct HealthPlugin;
 
 impl Plugin for HealthPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(AppState::MainMenu), reset_health)
+        app.insert_resource(Health(INITIAL_HEALTH))
             .add_event::<HealthReduceEvent>()
-            .add_systems(Update, reduce_health.run_if(in_state(GameState::InPlay)));
+            .add_systems(Update, reduce_health.run_if(in_state(GameState::InPlay)))
+            .add_systems(OnExit(AppState::InPlay), reset_health);
     }
 }
 
 #[derive(Event)]
 pub struct HealthReduceEvent;
 
-#[derive(Component)]
+#[derive(Resource)]
 struct Health(u8);
 
-fn reset_health(mut commands: Commands, mut health_query: Query<&mut Health>) {
-    if health_query.is_empty() {
-        commands.spawn(Health(INITIAL_HEALTH));
-        return;
-    }
-    let mut health = health_query.get_single_mut().unwrap();
-    health.0 = INITIAL_HEALTH;
+fn reset_health(mut health: ResMut<Health>) {
+    health.0 = INITIAL_HEALTH
 }
 
 fn reduce_health(
     mut health_reduce_event: EventReader<HealthReduceEvent>,
-    mut health_query: Query<&mut Health>,
+    mut health: ResMut<Health>,
 ) {
     for _ in health_reduce_event.read() {
-        let mut health = health_query.get_single_mut().unwrap();
         health.0 -= 1;
     }
 }
