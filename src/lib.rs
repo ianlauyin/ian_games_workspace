6935685e37c7039b3ab93f8b6414a13b3859b077
ianlau <ianlauyin@gmail.com>
2024-10-22 14:52:15 +0800

    Update lib

---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+use bevy::prelude::*;
+use bevy_embedded_assets::EmbeddedAssetPlugin;
+use wasm_bindgen::prelude::wasm_bindgen;
+
+use asset_loader::AssetPlugin;
+use setup::SetupPlugin;
+use ui::BackgroundPlugin;
+
+use crate::ui::{MainMenuPlugin, ResultPlugin};
+use crate::util::VelocityPlugin;
+
+mod asset_loader;
+mod game;
+mod setup;
+mod states;
+mod ui;
+mod util;
+
+#[wasm_bindgen]
+pub fn start() {
+    let ui_plugins = (
+        AssetPlugin,
+        SetupPlugin,
+        BackgroundPlugin,
+        MainMenuPlugin,
+        ResultPlugin,
+    );
+
+    let game_plugins = (
+        game::HealthPlugin,
+        game::SpaceshipPlugin,
+        game::BulletPlugin,
+        game::UFOPlugin,
+        game::ScorePlugin,
+        game::CollisionPlugin,
+        game::ExplosionPlugin,
+        game::InvisiblePlugin,
+    );
+
+    let util_plugins = VelocityPlugin;
+
+    App::new()
+        .add_plugins(EmbeddedAssetPlugin::default())
+        .add_plugins(ui_plugins)
+        .add_plugins(game_plugins)
+        .add_plugins(util_plugins)
+        .run();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,5 @@
 #![windows_subsystem = "windows"]
 
-use bevy::prelude::*;
-use bevy_embedded_assets::EmbeddedAssetPlugin;
-
-use asset_loader::AssetPlugin;
-use setup::SetupPlugin;
-use ui::BackgroundPlugin;
-
-use crate::ui::{MainMenuPlugin, ResultPlugin};
-use crate::util::VelocityPlugin;
-
-mod asset_loader;
-mod game;
-mod setup;
-mod states;
-mod ui;
-mod util;
-
 fn main() {
-    let ui_plugins = (
-        AssetPlugin,
-        SetupPlugin,
-        BackgroundPlugin,
-        MainMenuPlugin,
-        ResultPlugin,
-    );
-
-    let game_plugins = (
-        game::HealthPlugin,
-        game::SpaceshipPlugin,
-        game::BulletPlugin,
-        game::UFOPlugin,
-        game::ScorePlugin,
-        game::CollisionPlugin,
-        game::ExplosionPlugin,
-        game::InvisiblePlugin,
-    );
-
-    let util_plugins = VelocityPlugin;
-
-    App::new()
-        .add_plugins(EmbeddedAssetPlugin::default())
-        .add_plugins(ui_plugins)
-        .add_plugins(game_plugins)
-        .add_plugins(util_plugins)
-        .run();
+    shooting_game_lib::start();
 }
